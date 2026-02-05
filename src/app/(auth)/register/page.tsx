b9d9a0ef2ca60364baad12ed7fce1ea7f263a4c7
ChristianQ <christianq@cloudstaff.com>
2026-02-05 14:21:40 +0800

    update

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,3 +1,27 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Register() {
-    return <h1>Register</h1>
+    const [input, setInput] = useState({ name: '', email: ''});
+    
+        const handleChange = (e: any) => {
+            setInput({ ...input, [e.target.name]: e.target.value});
+        };
+    
+        const handleSubmit = async (e: any) => {
+            e.preventDefault();
+            
+            console.log(input)
+        }
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <input name="name" placeholder="Name" onChange={handleChange} />
+                <input name="email" placeholder="Email" onChange={handleChange} />
+                <button type="submit">Submit</button>
+            </form>
+        </>
+    )
 }
