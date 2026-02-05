87f5258e2aeab2654687321cce11bcbdae4a50cf
ChristianQ <christianq@cloudstaff.com>
2026-02-05 14:17:23 +0800

    initializing repo

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Login() {
+    const [input, setInput] = useState({ name: '', email: ''});
+
+    const handleChange = (e: any) => {
+        setInput({ ...input, [e.target.name]: e.target.value});
+    };
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+        
+        console.log(input)
+    }
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <input name="name" placeholder="Name" onChange={handleChange} />
+                <input name="email" placeholder="Email" onChange={handleChange} />
+                <button type="submit">Submit</button>
+            </form>
+        </>
+    )
+}
